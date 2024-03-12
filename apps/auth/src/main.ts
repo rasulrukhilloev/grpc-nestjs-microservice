bb5b6209a18271312838700cb2e83c6622698ddd
Rass <rukhillaevrasul@gmail.com>
2024-03-12 17:49:31 +0500

    first

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AUTH_PACKAGE_NAME } from '@app/common';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../auth.proto'),
+        package: AUTH_PACKAGE_NAME,
+        // url: '0.0.0.0:5000',
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
